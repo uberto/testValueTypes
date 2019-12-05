afa2036f526e039f8c5743f2a62f2274069cea41
uberto <uberto.gama@gmail.com>
2019-12-05 23:02:13 +0000

    add ministring

---
+++ src/main/java/com/ubertob/ministring/MiniString.java
@@ -0,0 +1,58 @@
+package com.ubertob.ministring;
+
+
+inline public class MiniString {
+
+    long raw;
+
+    public MiniString(String str) {
+        raw = encode(str);
+    }
+
+    public String get() {
+        return decode(raw);
+    }
+
+    public static final int MAX_MINISTR_LEN = 10;
+    public static final int MINI_STR_BASE = 64;
+    public static final String letters = "=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 _-!?.$&%@#:[]{}()*<>:;',/^";
+
+    private static long encode(String str) {
+        String prepared = prepareString(str);
+        long encoded = 0;
+        for (char c : prepared.toCharArray()) {
+            int x = letters.indexOf(c);
+            encoded = encoded * MINI_STR_BASE + x;
+        }
+        return encoded;
+    }
+
+    private static String prepareString(String str) {
+        StringBuilder prepared = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (letters.indexOf(c) >= 0)
+                prepared.append(c);
+            if (prepared.length() > MAX_MINISTR_LEN)
+                break;
+        }
+        return prepared.toString();
+    }
+
+    private static String decode(long number) {
+        StringBuilder decoded = new StringBuilder();
+        long remaining = number;
+
+        while (true) {
+            int mod = (int) ( remaining % MINI_STR_BASE);
+            char c = letters.charAt(mod);
+            decoded.insert(0, c);
+            if ( remaining < MINI_STR_BASE)
+                break;
+            remaining = remaining / MINI_STR_BASE;
+        }
+        return decoded.toString();
+
+    }
+
+}
+
